c72bce83ac3922588b7fba2fa240cd55d09149d5
Jessica Klein <jess.klein@live.com>
2022-05-09 12:01:07 +0100

    Add files via upload

--- Scripts/QA/01 - Snippets/Dennis Login/Script1651824428631.groovy
+++ Scripts/QA/01 - Snippets/Dennis Login/Script1651824428631.groovy
@@ -1,1 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.apache.commons.lang.RandomStringUtils
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://auditor-qa.checkventory.com/login')
+
+WebUI.setText(findTestObject('Object Repository/Page_CheckVentory Auditor/input__MuiInputBase-input MuiOutlinedInput-input'), 
+    'dennis@checkventory.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_CheckVentory Auditor/input__MuiInputBase-input MuiOutlinedInput-_9272d3'), 
+    'Jln+94g971R5dNnL8hYLKgfOXRBjTjT+')
+
+WebUI.click(findTestObject('Object Repository/Page_CheckVentory Auditor/span_Login'))
 
